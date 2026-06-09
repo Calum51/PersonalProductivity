e4bca6ceeda41785e914b18c79943eaf14584474
Calum51 <jarod.malaval@gmail.com>
2026-06-09 12:41:43 +0200

    Initial commit

---
+++ src/main/java/fr/calum51/PersonalProductivity/PersonalProductivityApplication.java
@@ -0,0 +1,13 @@
+package fr.calum51.PersonalProductivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProductivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalProductivityApplication.class, args);
+	}
+
+}
